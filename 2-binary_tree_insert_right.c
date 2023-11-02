f4e29152845ff9dad26336a77304b64303e31f08
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-11-02 22:08:41 +0200

    adds descendents

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -18,13 +18,13 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
 
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->parent = parent;
-
 	if (new_node)
 	{
+		new_node->n = value;
+		new_node->left = NULL;
+		new_node->right = NULL;
+		new_node->parent = parent;
+
 		if (parent->right == NULL)
 			parent->right = new_node;
 		else
